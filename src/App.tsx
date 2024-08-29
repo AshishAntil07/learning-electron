61ebd86b0f110b52e05bdb11d6bae2e1f8b64f53
Ashish <ashishantil.antil07@gmail.com>
2024-08-29 23:07:56 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="h-screen w-screen bg-blue-950 text-white grid place-items-center font-mono text-8xl">
+      glhf ;)
+    </div>
+  );
+}
+
+export default App;
